413211d390c52b6aed1097d1240ad1cb70004130
Alex Gaspar <contato.cubecraft@gmail.com>
2017-06-30 17:12:29 -0300

    NavBar2 and Refine Functions

--- js/cube.js
+++ js/cube.js
@@ -1,12 +1,37 @@
-/* Effect for NavTop Model 1 */
-$(window).on('scroll',function(){
+/* Functions on Load */
+window.onload = function() {
+  relogo();
+};
 
-    position = Math.round($(window).scrollTop());
-    if (position > 50) {
-		$('nav').addClass('change-navtop-model1');
-		$('nav ul li').addClass('change-li');
-    } else {
-		$('nav').removeClass('change-navtop-model1');
-		$('nav ul li').removeClass('change-li');
-   }
+/* Functions on Scroll */
+$(window).on('scroll',function(){
+  renav2();
 });
+
+
+
+/* Functions */
+
+/* Resize the logo */
+function relogo(){
+  if ($('nav').hasClass('navtop-model1')){
+    $('#logoimg').addClass('logo-nav1');
+  }
+  if ($('nav').hasClass('navtop-model2')){
+    $('#logoimg').addClass('logo-nav12');
+  } 
+}
+
+/* Effect for NavTop Model 2 */
+function renav2(){
+  position = Math.round($(window).scrollTop());
+    if (position > 50 && $('nav').hasClass('navtop-model2')) {
+      $('nav').addClass('change-navtop-model2');
+      $('nav ul li').addClass('change-li2');
+      } else {
+      $('nav').removeClass('change-navtop-model2');
+      $('nav ul li').removeClass('change-li2');
+   }
+}
+
+/* End of Functions */
